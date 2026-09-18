b0729e629f2d77f4ec6934fa53da2fba38066267
001zombie110-sudo <001zombie110@gmail.com>
2026-09-18 19:24:08 +0700

    Add files via upload

---
+++ lat2.js
@@ -0,0 +1,23 @@
+// === sistemm nilai === //
+
+const nilai = 85;
+const status = "ANDA LULUS SANGAT MEMUASKAN";
+
+// === aritmatika === //
+if(nilai >= 85){
+    console.log ("ANDA LULUS SANGAT MEMUASKAN")
+}
+else if(nilai >= 70){
+    console.log("ANDA LULUS")
+}
+else if(nilai >= 60){
+    console.log("LULUS CUKUP")
+}
+else {
+    console.log("ANDA TIDAK LULUS")
+}
+
+// === hasil output === //
+
+console.log("nilai = " + nilai);
+console.log("status = " + status);
